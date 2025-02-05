886ae2f64378746d7b94eebd3c0350e6bd2f12e5
williamvg93 <sonico_5071@hotmail.com>
2025-02-04 23:17:30 -0500

    feat: :sparkles: Creación de interfaces para cada entidad, para la unitofwork y para el repositorio generico

---
+++ API/Domain/Interfaces/IProduct.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiGestionFacturas;
+
+namespace Domain.Interfaces
+{
+    public interface IProduct : IGenericRepository<Product>
+    {
+        
+    }
+}
---
+++ API/Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IClient Clients {get;}
+        IProduct Products {get;}
+        IInvoice Invoices {get;}
+        IInvoiceDetail InvoiceDetails {get;}
+        Task<int> SaveAsync();
+    }
+}
