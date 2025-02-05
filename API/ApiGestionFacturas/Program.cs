6a3dac2abe46978975bae3fef11db75616c0a852
williamvg93 <sonico_5071@hotmail.com>
2025-02-05 15:20:16 -0500

    feat: :wrench: Agregando nuevas dependencias y sus configuraciones a la app

--- API/ApiGestionFacturas/Program.cs
+++ API/ApiGestionFacturas/Program.cs
@@ -1,4 +1,7 @@
+using System.Reflection;
 using ApiGestionFacturas;
+using ApiGestionFacturas.Extensions;
+using AspNetCoreRateLimit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +11,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Agregando los controlodores al contenedor de dependencias
+builder.Services.AddControllers();
+
+// Agregando los cors y sus configuraciones al contenedor de dependencias
+builder.Services.ConfigureCors();
+
+// Agregando el rateLimit y sus configuraciones al contenedor de dependencias
+builder.Services.ConfigureRatelimiting();
+
+// Agregando el AutoMapper para los Dtos al contenedor de dependencias y expesificando el metodo de busqueda de Dtos
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
+
+// Agregando otras dependencias necesarias como la UnitOfWork al contenedor de dependencias
+builder.Services.AddAplicationServices();
+
+// Agregando la conexion a la base de datos y sus credenciales de acceso al contenedor de dependencias
 builder.Services.AddDbContext<ApiGestionFacturasContext>(options => {
     string connectionStrings = builder.Configuration.GetConnectionString("SqlServerConne");
     options.UseSqlServer(connectionStrings);
@@ -23,6 +42,16 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Utilizando el rateLimiting
+app.UseIpRateLimiting();
+
+// Mapeando los controladores y activando sus rutas 
+app.MapControllers();
+
+// Habilitar los cors como fueron configurados(CorsPolicy) en la app
+app.UseCors("CorsPolicy");
+
 
 app.Run();
 
